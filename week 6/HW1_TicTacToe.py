326456f59d61a4903c0b27d872be3fbdfc22e11d
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-09 18:52:38 +0330

    the game is completed

--- week 6/HW1_TicTacToe.py
+++ week 6/HW1_TicTacToe.py
@@ -7,17 +7,70 @@ def printEnv(A):
         print(f" {i[0]} | {i[1]} | {i[2]}")
         print("-----------")
 
-env = [' ', ' ', 'O', ' ', ' ', ' ', ' ', ' ', ' ']
+
+def statusCheck(possibleMoves, environment):
+    if len(possibleMoves) == 0:
+        return "No Winner!"
+    
+    else:
+        B = np.reshape(environment, [3, 3])
+        ref_user = ['X', 'X', 'X']
+        ref_pc = ['O', 'O', 'O']
+
+        for i in B:
+            if list(i) == ref_user:
+                return "YOU ARE THE WINNER"
+            elif list(i) == ref_pc:
+                return "THE PC IS THE WINNER!"
+        
+        C = B . T
+        for i in C:
+            if list(i) == ref_user:
+                return "YOU ARE THE WINNER"
+            elif list(i) == ref_pc:
+                return "THE PC IS THE WINNER!"
+        
+        diag = [B[0, 0], B[1, 1], B[2, 2]]
+        if diag == ref_user:
+            return "YOU ARE THE WINNER"
+        elif diag == ref_pc:
+            return "THE PC IS THE WINNER!"
+        
+        diag = [B[0, 2], B[1, 1], B[2, 0]]
+        if diag == ref_user:
+            return "YOU ARE THE WINNER"
+        elif diag == ref_pc:
+            return "THE PC IS THE WINNER!"
+
+    return "OK"
+    
+
+env = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 printEnv(env)
 
 status = "OK"
 while status == "OK":
-    user = int(input(f"choose from {possible}.    "))
-    env[user-1] = "X"
-    possible.remove(user)
+    status = statusCheck(possible, env)
+    if status != "OK":
+        break
+    else:
+        user = int(input(f"choose from {possible}.    "))
+        env[user-1] = "X"
+        possible.remove(user)
+        printEnv(env)
+        print("****************************")
+    
 
-    printEnv(env)
+    status = statusCheck(possible, env)
+    if status != "OK":
+        break
+    else:
+        pc = random.choices(possible)[0]
+        env[pc-1] = "O"
+        possible.remove(pc)
+        printEnv(env)
+        print("****************************")
 
-    pc = 
+print(status)
