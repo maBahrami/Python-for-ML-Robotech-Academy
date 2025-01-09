36e867a56cc29080be5b4c1b008f61a4674fbed0
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-09 10:27:16 +0330

    numpy file completed

--- review/numpy.py
+++ review/numpy.py
@@ -45,9 +45,111 @@ np.load("myFile.txt")       # it is better to use pandas for this matter
 
 #----------------------------------------------------------------
 
+a.shape
+
+len(a)
+
+a.ndim
+
+a.size
+
+a.dtype
+
+a.astype(int)
+
+# --------------------- data types in numpy array ------------------------
+a = np.array([1, 2, 3], dtype = int)      #int
+
+a = np.array([1, 2, 3], dtype = float)    #float
+
+a = np.array([1, 2, 3], dtype = complex)    # complex
+
+a = np.array([1, 1, 0], dtype = bool)       # bool
+
+# ------------------- mathematical operation in arrays -----------------
+g = a - b
+
+h = a + b
+
+g = a / b  # each element is divided by the corresponfing element (it is not atrix multipilication)
+
+g = a * b  
+
+
+np.exp(a)       # exp() of each element will be calculated
+
+np.sqrt(a)      # sqrt() of each element will be calculated
+
+np.sin(a)       # sin() of each element will be calculated
+
+np.cos(a)
+
+np.log(a)
 
 
 
+c = np.matmul(a, b)     # matrix multiplication
 
+c = a @ b       # short form of the matrix multiplication
+
+
+
+a == b          # elementwise arrays comparison
+
+a < 2           # elementwise (a is an array)
+
+
+np.sum(a, axis=0)
+np.sum(a, axis=1)
+
+
+# ---------------------- copying ----------------------------
+a = np.array([1, 2, 3, 4, 5])
+
+x = a.copy()
+
+a[0] = 100      # just a willl be changed without affecting x
+
+# ------------------------------------------------------------
+a = np.array([[3, 6, 7, 1], [2, 6, 9, 4]])
+b = np.sort(a, axis=0)
+c = np.axis(a, axis=1)
+
+
+
+# --------------- slicing ------------------------------
+
+a = np.array([[1, 2, 3, 4, 5, 6],
+              [7, 8, 9, 3, 7, 9]])
+
+b = a[0:2, 1:4]
+
+
+b = a[::-1]  # reversing
+
+c = a[low : high : stride]
+
+
+# --------------- matrix transpose ----------------------
+
+b = np.transpose(a)
+
+b = a . T       # shorter form
+
+
+# -------------- reshape --------------------------------
+a = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+b = np.reshape(a, [3, 4])
+
+c = np.reshape(a, [2, -1])      # 2 rows and undifinite number of columns
+
+
+# ------------ concatenation -----------------------------
+c = np.concatenate((a, b), axis=1)
+
+c = np.hstack((a, b))
+
+c = np.vstack((a, b))
 
 
