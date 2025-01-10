ca5b31355647667226cea7defa3253e2eac8e298
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-10 10:24:22 +0330

    HW5 started but not finished. simple. time waister

---
+++ week 6/HW5.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+data = {"Java": 22.2, "Python": 17.6, "PHP": 8.8, "JavaScript": 8, "C#": 7.7, "C++": 6.7}
+
+x = list(data.values())
+y = (list(data.keys()))
+print(x)
+print(y)
+
+plt.bar(y, x)
+
+plt.show()
