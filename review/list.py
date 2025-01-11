78a681aff929c1e247a42e2ede525b2c25cae671
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-11 07:20:41 +0330

    sorted function added

--- review/list.py
+++ review/list.py
@@ -56,5 +56,26 @@ a.reverse()
 # paye careful attention to the fact that all of the methods do their function in plance
 
 
+# ------------------------- sorted function -----------------------------------
+def secondElement(i):
+    return i[1]
+
+a = [2, 9, 8, 5, 6, 7, 1, 10, 11, 12, 17, 5, ]
+b = ['a', 'dv', 'absgf', 'w', 'wwr', 'ewerer']
+c = [(1, 5), (5, 2), (6, 4), (3, 2), (8, 9)]
+
+sortedArrayA = sorted(b, reverse = True)
+
+sortedArrayB = sorted(b, key = len, reverse = True)
+
+sortedArrayC = sorted(c, key = secondElement)
+
+sortedArrayC2 = sorted(c, key = lambda i:i[1])      # using lambda instead of defining a seperate function
+
+
+
+print(sortedArrayC2)
+
+# --------------------------------------------------------------------------------
 
 
