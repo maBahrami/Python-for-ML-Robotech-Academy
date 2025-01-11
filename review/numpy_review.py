2716945ffe23e711bfd5051d5315f1cfd82e3812
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-11 07:52:15 +0330

    review files renamed

--- review/numpy_review.py
+++ review/numpy_review.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 a = np.array([1, 2, 3])         # a is a 1*3 matrix
 
 b = np.array([[1, 2, 3], 
---
+++ review/pandas_review.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+cities = {
+            "A": 32,
+            "B": 18,
+            "C": 22,
+            "D": 30
+        }
+
+ct = pd.Series(cities)
+print(cities)
+print(ct)
