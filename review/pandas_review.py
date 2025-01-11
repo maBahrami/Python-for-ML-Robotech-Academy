c209b62bd5aec94977e148f3ea42f07ca83d8ef0
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-11 09:40:00 +0330

    pandas review has been created. pandas series investigated.

--- review/pandas_review.py
+++ review/pandas_review.py
@@ -1,12 +1,40 @@
 import pandas as pd
-
+'''
 cities = {
-            "A": 32,
+            "A": 2,
             "B": 18,
             "C": 22,
             "D": 30
         }
 
+temp = [2, 18, 22, 30]
+
+# ------------------- Series -----------------------------
 ct = pd.Series(cities)
-print(cities)
-print(ct)
+
+t = pd.Series(temp)
+
+t2 = pd.Series(temp, index = ["A", "B", "C", "D"])
+
+print(ct.index)
+print(ct.values)
+
+# ------------------- element calling --------------------
+print(ct["A"])
+print(ct.A)
+print(ct.iloc[0])
+
+# ------------------- multi element calling -----------------
+print(ct[["A", "B"]])
+print(ct.iloc[[0, 3]])
+print(ct[0:3])
+print(ct["C":"D"])
+
+print(ct[[True, False, False, True]])
+
+# example
+print(f"the mean temp is {ct.mean()}")
+print(ct[ct >= ct.mean()])
+
+'''
+# ---------------------------- Data Frame --------------------------------------------
