8c7fca2ba170990ce058f7201c4fe5f997b325cd
mammadooWINDOWS <m.a.bahrami75@gmail.com>
2025-01-11 05:34:56 +0330

    the genral algorith is implemented. exceptions should be tested

---
+++ week 7/insertionSort.py
@@ -0,0 +1,21 @@
+a = [1, 5, 9, 6, 2, 4, 7, 0]
+
+for i in range(1, len(a)):
+    flag = True
+    k = i
+    j = k - 1
+    print(i, k, j)
+    while (flag == True) and (j > -1):
+        if a[k] < a[j]:
+            mem = a[j]
+            a[j] = a[k]
+            a[k] = mem
+
+            k -= 1
+            j = k -1 
+            print(a)
+        else:
+            flag = False
+
+print("\n---------------- * Finito * --------------------\n")
+print(a)
